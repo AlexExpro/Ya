798b8f624391401814b353ba7de5e38150904f07
Школа программирования 1 13 <schpr1-13@int.adtspb.ru>
2023-09-19 19:55:50 +0300

    python

---
+++ Python.py
@@ -0,0 +1,18 @@
+#matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+#matrix2 = [[9, 8, 7], [6, 5, 4], [3, 2, 1]] 
+#result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+#for i in range(len(matrix1)): 
+#         for j in range(len(matrix1[0])): 
+#                  result[i][j] = matrix1[i][j] + matrix2[i][j] 
+
+#print(result)
+Stolbec1 = int(input('Введите кол-во столбцов(1 матреца)'))
+liniya1 = int(input('Введите кол-во рядов(1 матреца)'))
+Stolbec2 = int(input('Введите кол-во столбцов(2 матреца)'))
+liniya2 = int(input('Введите кол-во рядов(2 матреца)'))
+
+cisla1 = int(input('Введите числа для столбцов(1 матрица)'))
+cisla2 = int(input('Введите числа для рядов(1 матрица)'))
+cisla3 = int(input('Введите числа для столбцов(2 матрица)'))
+cisla4 = int(input('Введите числа для рядов(2 матрица)'))
